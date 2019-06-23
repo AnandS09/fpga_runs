95f6818b5ea1c5000c0fea320f06f9cb5d6e5d19
Anand <anandsamajdar@gatech.edu>
2019-06-23 15:06:34 -0400

    typo correction

--- gather.py
+++ gather.py
@@ -6,7 +6,7 @@ cmd = "mkdir scale_runs_out"
 os.system(cmd)
 
 for i in range(1,11):
-    num_proc = 96 * 1
+    num_proc = 96 * i
 
     for df in df_list: 
         target_name = "./scale_runs/9x" + str(num_proc) + "_" + df + "/outputs/"
